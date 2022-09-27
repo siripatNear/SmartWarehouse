ce8599ddd9b656fe70b378632886ff30c36edeb1
WaenSalinthip <waen.salinthip@gmail.com>
2022-09-27 15:51:52 +0700

    install bcrypt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import TestBackend from './TestBackend';
 
 function App() {
   return (
     <div className="App">
-    <TestBackend />
     <h1>Hey, my name is Pathomporn.♥</h1>
+    <TestBackend />
     </div>
   );
 }
