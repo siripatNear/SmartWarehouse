605bf03f082436ef0726bd93a7f443ffd04806f4
WaenSalinthip <waen.salinthip@gmail.com>
2022-09-27 14:51:58 +0700

    Add database

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const Quote = require('inspirational-quotes');
-
 const app = express();
+const cors = require('cors');
 const port = 5000;
-//Allow frontend to reach backend [was blocked by core]
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-})
+
+//middle ware
+app.use(cors());
+app.use(express.json());
 
 //login path
 app.get('/login',(req, res) => {
