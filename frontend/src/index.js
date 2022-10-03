7b2fa932f9ebeca5e9c5349804822ab7dce2452c
siripatNear <siripat.0119@gmail.com>
2022-10-03 09:11:53 +0700

    Link navbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App, { router } from './App';
-import { ChakraProvider } from '@chakra-ui/react'
-import { RouterProvider } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App, { router } from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import { RouterProvider } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
+      <Navbar />
       <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 );
-
