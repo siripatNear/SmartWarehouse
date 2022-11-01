075cf93bba7faf913828d0e301412664d228a025
WaenSalinthip <waen.salinthip@gmail.com>
2022-11-01 21:17:25 +0700

    edit config.js

--- backend/database/config/config.js
+++ backend/database/config/config.js
@@ -1,23 +1,27 @@
+require('dotenv').config();
 module.exports = {
-  "development": {
-    "username": "postgres",
-    "password": "Slth_1811",
-    "database": "smartwarehouse",
-    "host": "localhost",
-    "dialect": "postgres"
+  development: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_NAME,
+    dialect: "postgres"
   },
-  "test": {
-    "username": "postgres",
-    "password": "Slth_1811",
-    "database": "smartwarehouse",
-    "host": "localhost",
-    "dialect": "postgres"
+  test: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_NAME,
+    dialect: "postgres"
   },
-  "production": {
-    "username": "postgres",
-    "password": "Slth_1811",
-    "database": "smartwarehouse",
-    "host": "localhost",
-    "dialect": "postgres"
+  production: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_NAME,
+    dialect: "postgres"
   }
 }
